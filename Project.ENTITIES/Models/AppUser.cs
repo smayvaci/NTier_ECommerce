eb25fb38ae877bc7c137b78594baf6d0d01b9ce2
SmA <121155503+smayvaci@users.noreply.github.com>
2023-05-17 20:48:37 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using Project.ENTITIES.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class AppUser:BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Guid ActivationCode { get; set; } //Kullanıcıya gönderilen Uniq aktivasyon kodudur.
+        public bool Active { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+
+        //Relational Properties
+        public virtual AppUserProfile Profile { get; set; }
+        public virtual List<Order> Orders { get; set; }
+    }
+}
